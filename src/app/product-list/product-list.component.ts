7ca145416d5054bf7b95ddaf67bb1511443e1f1a
awilliams62090 <awilliams62090@gmail.com>
2020-01-25 15:50:21 -0700

    Adding Output decorator and onNotify function to Notify Me button

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  onNotify(){
+    window.alert('We will notify you when this product goies on sale!')
+  }
 }
 
 
