411a1de072c1cbf6f5bb9b1835aca42bc7921bab
awilliams62090 <awilliams62090@gmail.com>
2020-01-26 16:45:42 -0700

    Added cart Service and imported to product-details

---
+++ src/app/cart.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CartService {
+
+  items =[];
+
+//push items into shopping cart item array defined above 
+  addToCart(){
+    this.items.push();
+  }
+
+//return items array 
+  getItems(){
+    return this.items; 
+  }
+
+//empty the shopping cart by setting items back to an empty array 
+  clearCart(){
+    this.items= [];
+    return this.items; 
+  }
+
+  constructor() { }
+
+}
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from './../products';
+import { CartService} from './../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -13,6 +14,7 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService,
   ) { }
 
   ngOnInit() {
