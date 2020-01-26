091809f4beb25a84cf66ae9916e0ddcf92d8861f
awilliams62090 <awilliams62090@gmail.com>
2020-01-25 18:29:14 -0700

    Fixing router and product detail page

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { products } from '../products';
+import { products } from './../products';
 
 @Component({
   selector: 'app-product-details',
