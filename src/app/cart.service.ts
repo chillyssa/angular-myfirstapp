06b135c19debd19099fb630847ba39c89c9be6a8
awilliams62090 <awilliams62090@gmail.com>
2020-01-26 18:03:08 -0700

    added shipping component and added HttpClient module to get shipping functionality

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,29 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-
 export class CartService {
+  items = [];
 
-  items =[];
+  constructor(
+    private http: HttpClient
+    ) {}
 
-//push items into shopping cart item array defined above 
-  addToCart(product){
+  //push items into shopping cart item array defined above
+  addToCart(product) {
     this.items.push(product);
   }
 
-//return items array 
-  getItems(){
-    return this.items; 
+  //return items array
+  getItems() {
+    return this.items;
   }
 
-//empty the shopping cart by setting items back to an empty array 
-  clearCart(){
-    this.items= [];
-    return this.items; 
+  //empty the shopping cart by setting items back to an empty array
+  clearCart() {
+    this.items = [];
+    return this.items;
   }
 
-  constructor() { }
-
+  //Uses http client get() method to retrieve shipping data from shipping.json in assets folder
+  getShippingPrices(){
+    return this.http.get('./assets/shipping.json');
+  }
+  
 }
