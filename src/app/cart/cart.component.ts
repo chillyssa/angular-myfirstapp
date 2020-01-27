c43a6c9ea8ba3aebb622f5ce594edcb10e95edb3
awilliams62090 <awilliams62090@gmail.com>
2020-01-26 17:28:31 -0700

    Changed Cart template to display cart and added cartService to cart.ts file

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CartService} from './../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  
+  items;
 
-  constructor() { }
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
+    this.items = this.cartService.getItems();
   }
 
 }
